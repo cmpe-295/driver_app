4c1e5bec01daf5953bf57263e97a81410fb736a3
priyankatummala <priyanka.t.88@gmail.com>
2016-10-19 00:33:27 -0700

    Integrated firebase messaging, added Dex support, Login UI Changed

---
+++ Driver/app/src/main/java/spartansaferide/sjsu/edu/driver/AndroidFirebaseMessageService.java
@@ -0,0 +1,52 @@
+package spartansaferide.sjsu.edu.driver;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by priyanka.tummala on 10/18/16.
+ */
+public class AndroidFirebaseMessageService extends FirebaseMessagingService {
+
+    private static final String TAG = "SafeRideMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        //Log data to Log Cat
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        //create notification
+        createNotification(remoteMessage.getNotification().getBody());
+    }
+
+    private void createNotification( String messageBody) {
+        Intent intent = new Intent( this , DriverMapsActivity.class );
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent resultIntent = PendingIntent.getActivity( this , 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri notificationSoundURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder mNotificationBuilder = new NotificationCompat.Builder( this)
+                .setSmallIcon(R.mipmap.spartan_logo)
+                .setContentTitle("SafeRide")
+                .setContentText(messageBody)
+                .setAutoCancel( true )
+                .setSound(notificationSoundURI)
+                .setContentIntent(resultIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, mNotificationBuilder.build());
+    }
+
+}
