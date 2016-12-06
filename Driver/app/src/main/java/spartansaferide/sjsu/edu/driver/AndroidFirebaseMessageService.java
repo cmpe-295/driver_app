98ce310656009b8587af53f438b9ff515c39cfc2
balajianoopgupta <balajianoopgupta.ba@gmail.com>
2016-12-06 02:49:09 -0800

    Removed unused parameter in createNotification function

--- Driver/app/src/main/java/spartansaferide/sjsu/edu/driver/AndroidFirebaseMessageService.java
+++ Driver/app/src/main/java/spartansaferide/sjsu/edu/driver/AndroidFirebaseMessageService.java
@@ -1,11 +1,5 @@
 package spartansaferide.sjsu.edu.driver;
 
-import android.app.Service;
-import android.content.Intent;
-import android.util.Log;
-
-import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -13,11 +7,9 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
-import org.json.JSONObject;
 
 /**
  * Created by priyanka.tummala on 10/18/16.
@@ -29,30 +21,25 @@ public class AndroidFirebaseMessageService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        //Log data to Log Cat
-//        Log.d(TAG, "From: " + remoteMessage.getFrom());
-//        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
-        //create notification
-        createNotification(remoteMessage.getNotification().getBody());
+
+        createNotification();
         String notification_body = remoteMessage.getNotification().getBody();
 
-        Notification.getInstance().message=notification_body;
+        Notification.getInstance().message = notification_body;
     }
 
-
-
-    private void createNotification( String messageBody) {
-        Intent intent = new Intent( this , DriverMapsActivity.class );
+    private void createNotification() {
+        Intent intent = new Intent(this, DriverMapsActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent resultIntent = PendingIntent.getActivity( this , 0, intent,
+        PendingIntent resultIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri notificationSoundURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder mNotificationBuilder = new NotificationCompat.Builder( this)
+        NotificationCompat.Builder mNotificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.spartan_logo)
                 .setContentTitle("SafeRide")
                 .setContentText("You have a new ride!")
-                .setAutoCancel( true )
+                .setAutoCancel(true)
                 .setSound(notificationSoundURI)
                 .setContentIntent(resultIntent);
 
@@ -61,10 +48,4 @@ public class AndroidFirebaseMessageService extends FirebaseMessagingService {
 
         notificationManager.notify(0, mNotificationBuilder.build());
     }
-
-
-    public interface SetRoute{
-        public void parseNotifcation(String N);
-    }
-
 }
